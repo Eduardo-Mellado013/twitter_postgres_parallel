12ce3bf81c9d70f78532f7ae3dd9cdaaf8866178
Eduardo_Mellado013 <eduardom71891@gmail.com>
2025-04-21 23:04:42 -0700

    Updated load_tweets_parallel.sh

--- load_normalized.sh
+++ load_normalized.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
 
 file=$1
-python3 load_tweets.py --db "postgresql://postgres:pass@localhost:6344/pos    tgres" --inputs "$file"
+python3 load_tweets.py --db "postgresql://postgres:pass@localhost:6344/postgres" --inputs "$file"
