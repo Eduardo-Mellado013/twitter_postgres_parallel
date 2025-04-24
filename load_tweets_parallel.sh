5e11bb7897a607fa23032b6df7b70a9e39c4ed7f
Eduardo_Mellado013 <eduardom71891@gmail.com>
2025-04-23 17:21:11 -0700

    Updated load_tweets_parallel.sh

--- load_tweets_parallel.sh
+++ load_tweets_parallel.sh
@@ -6,16 +6,20 @@ echo '==========================================================================
 echo 'load pg_denormalized'
 echo '================================================================================'
 # FIXME: implement this with GNU parallel
-
-time echo "$files" | parallel ./load_denormalized.sh
+time for file in $files; do
+    echo
+    parallel ./load_denormalized.sh
+done
 
 echo '================================================================================'
 echo 'load pg_normalized'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
-time echo "$files" | parallel ./load_normalized.sh
+time for file in $files; do
+    parallel ./load_normalized.sh
+done
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
-# FIXME: implement this with GNU parallel
-time echo "$files" | parallel ./load_normalized_batch.sh
+time for file in $files; do
+    parallel ./load_normalized_batch.sh
+done
